9adcf370c71e3f888d7bcdb0d4e1f45434f0af64
cannaredrd <114183849+cannaredrd@users.noreply.github.com>
2023-04-22 20:42:19 +0000

    actualizar cronologia

--- dist/index.8d9388f3.js
+++ dist/index.8d9388f3.js
@@ -867,10 +867,10 @@ function _inheritsLoose(subClass, superClass) {
     subClass.__proto__ = superClass;
 }
 /*!
- * GSAP 3.11.4
+ * GSAP 3.11.5
  * https://greensock.com
  *
- * @license Copyright 2008-2022, GreenSock. All rights reserved.
+ * @license Copyright 2008-2023, GreenSock. All rights reserved.
  * Subject to the terms at https://greensock.com/standard-license or for
  * Club GreenSock members, the agreement issued with that membership.
  * @author: Jack Doyle, jack@greensock.com
@@ -1473,7 +1473,11 @@ distribute = function distribute(v) {
     animation.scrollTrigger && animation.scrollTrigger.kill(!!_reverting);
     animation.progress() < 1 && _callback(animation, "onInterrupt");
     return animation;
-}, _quickTween, _createPlugin = function _createPlugin(config) {
+}, _quickTween, _registerPluginQueue = [], _createPlugin = function _createPlugin(config) {
+    if (!_windowExists()) {
+        _registerPluginQueue.push(config);
+        return;
+    }
     config = !config.name && config["default"] || config; //UMD packaging wraps things oddly, so for example MotionPathHelper becomes {MotionPathHelper:MotionPathHelper, default:MotionPathHelper}.
     var name = config.name, isFunc = _isFunction(config), Plugin = name && !isFunc && config.init ? function() {
         this._props = [];
@@ -1773,6 +1777,7 @@ _hue = function _hue(h, m1, m2) {
                     (_win.gsapVersions || (_win.gsapVersions = [])).push(gsap.version);
                     _install(_installScope || _win.GreenSockGlobals || !_win.gsap && _win || {});
                     _raf = _win.requestAnimationFrame;
+                    _registerPluginQueue.forEach(_createPlugin);
                 }
                 _id && _self.sleep();
                 _req = _raf || function(f) {
@@ -2054,7 +2059,7 @@ var Animation = /*#__PURE__*/ function() {
         // prioritize rendering where the parent's playhead lines up instead of this._tTime because there could be a tween that's animating another tween's timeScale in the same rendering loop (same parent), thus if the timeScale tween renders first, it would alter _start BEFORE _tTime was set on that tick (in the rendering loop), effectively freezing it until the timeScale tween finishes.
         this._rts = +value || 0;
         this._ts = this._ps || value === -_tinyNum ? 0 : this._rts; // _ts is the functional timeScale which would be 0 if the animation is paused.
-        this.totalTime(_clamp(-this._delay, this._tDur, tTime), true);
+        this.totalTime(_clamp(-Math.abs(this._delay), this._tDur, tTime), true);
         _setEnd(this); // if parent.smoothChildTiming was false, the end time didn't get updated in the _alignPlayhead() method, so do it here.
         return _recacheAncestors(this);
     };
@@ -2318,7 +2323,7 @@ var Timeline = /*#__PURE__*/ function(_Animation) {
                     time > dur && (time = dur);
                 }
                 prevIteration = _animationCycle(this._tTime, cycleDuration);
-                !prevTime && this._tTime && prevIteration !== iteration && (prevIteration = iteration); // edge case - if someone does addPause() at the very beginning of a repeating timeline, that pause is technically at the same spot as the end which causes this._time to get set to 0 when the totalTime would normally place the playhead at the end. See https://greensock.com/forums/topic/23823-closing-nav-animation-not-working-on-ie-and-iphone-6-maybe-other-older-browser/?tab=comments#comment-113005
+                !prevTime && this._tTime && prevIteration !== iteration && this._tTime - prevIteration * cycleDuration - this._dur <= 0 && (prevIteration = iteration); // edge case - if someone does addPause() at the very beginning of a repeating timeline, that pause is technically at the same spot as the end which causes this._time to get set to 0 when the totalTime would normally place the playhead at the end. See https://greensock.com/forums/topic/23823-closing-nav-animation-not-working-on-ie-and-iphone-6-maybe-other-older-browser/?tab=comments#comment-113005 also, this._tTime - prevIteration * cycleDuration - this._dur <= 0 just checks to make sure it wasn't previously in the "repeatDelay" portion
                 if (yoyo && iteration & 1) {
                     time = dur - time;
                     isYoyo = 1;
@@ -2368,7 +2373,7 @@ var Timeline = /*#__PURE__*/ function(_Animation) {
                 this._zTime = totalTime;
                 prevTime = 0; // upon init, the playhead should always go forward; someone could invalidate() a completed timeline and then if they restart(), that would make child tweens render in reverse order which could lock in the wrong starting values if they build on each other, like tl.to(obj, {x: 100}).to(obj, {x: 0}).
             }
-            if (!prevTime && time && !suppressEvents) {
+            if (!prevTime && time && !suppressEvents && !iteration) {
                 _callback(this, "onStart");
                 if (this._tTime !== tTime) // in case the onStart triggered a render at a different spot, eject. Like if someone did animation.pause(0.5) or something inside the onStart.
                 return this;
@@ -3091,7 +3096,7 @@ var Tween = /*#__PURE__*/ function(_Animation2) {
             }
             this.ratio = ratio = (yoyoEase || this._ease)(time / dur);
             if (this._from) this.ratio = ratio = 1 - ratio;
-            if (time && !prevTime && !suppressEvents) {
+            if (time && !prevTime && !suppressEvents && !iteration) {
                 _callback(this, "onStart");
                 if (this._tTime !== tTime) // in case the onStart triggered a render at a different spot, eject. Like if someone did animation.pause(0.5) or something inside the onStart.
                 return this;
@@ -3777,17 +3782,17 @@ var gsap = _gsap.registerPlugin({
         while(i--)this.add(target, i, target[i] || 0, value[i], 0, 0, 0, 0, 0, 1);
     }
 }, _buildModifierPlugin("roundProps", _roundModifier), _buildModifierPlugin("modifiers"), _buildModifierPlugin("snap", snap)) || _gsap; //to prevent the core plugins from being dropped via aggressive tree shaking, we must include them in the variable declaration in this way.
-Tween.version = Timeline.version = gsap.version = "3.11.4";
+Tween.version = Timeline.version = gsap.version = "3.11.5";
 _coreReady = 1;
 _windowExists() && _wake();
 var Power0 = _easeMap.Power0, Power1 = _easeMap.Power1, Power2 = _easeMap.Power2, Power3 = _easeMap.Power3, Power4 = _easeMap.Power4, Linear = _easeMap.Linear, Quad = _easeMap.Quad, Cubic = _easeMap.Cubic, Quart = _easeMap.Quart, Quint = _easeMap.Quint, Strong = _easeMap.Strong, Elastic = _easeMap.Elastic, Back = _easeMap.Back, SteppedEase = _easeMap.SteppedEase, Bounce = _easeMap.Bounce, Sine = _easeMap.Sine, Expo = _easeMap.Expo, Circ = _easeMap.Circ;
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"l02JQ":[function(require,module,exports) {
 /*!
- * CSSPlugin 3.11.4
+ * CSSPlugin 3.11.5
  * https://greensock.com
  *
- * Copyright 2008-2022, GreenSock. All rights reserved.
+ * Copyright 2008-2023, GreenSock. All rights reserved.
  * Subject to the terms at https://greensock.com/standard-license or for
  * Club GreenSock members, the agreement issued with that membership.
  * @author: Jack Doyle, jack@greensock.com
@@ -3845,7 +3850,9 @@ _renderRoundedCSSProp = function _renderRoundedCSSProp(ratio, data) {
             ~property.indexOf(",") ? property.split(",").forEach(function(a) {
                 return _this.tfm[a] = _get(target, a);
             }) : this.tfm[property] = target._gsap.x ? target._gsap[property] : _get(target, property); // note: scale would map to "scaleX,scaleY", thus we loop and apply them both.
-        }
+        } else return _propertyAliases.transform.split(",").forEach(function(p) {
+            return _saveStyle.call(_this, p, isNotCSS);
+        });
         if (this.props.indexOf(_transformProp) >= 0) return;
         if (target._gsap.svg) {
             this.svgo = target.getAttribute("data-svg-origin");
@@ -3863,7 +3870,7 @@ _renderRoundedCSSProp = function _renderRoundedCSSProp(ratio, data) {
 }, _revertStyle = function _revertStyle() {
     var props = this.props, target = this.target, style = target.style, cache = target._gsap, i, p;
     for(i = 0; i < props.length; i += 3)// stored like this: property, isNotCSS, value
-    props[i + 1] ? target[props[i]] = props[i + 2] : props[i + 2] ? style[props[i]] = props[i + 2] : style.removeProperty(props[i].replace(_capsExp, "-$1").toLowerCase());
+    props[i + 1] ? target[props[i]] = props[i + 2] : props[i + 2] ? style[props[i]] = props[i + 2] : style.removeProperty(props[i].substr(0, 2) === "--" ? props[i] : props[i].replace(_capsExp, "-$1").toLowerCase());
     if (this.tfm) {
         for(p in this.tfm)cache[p] = this.tfm[p];
         if (cache.svg) {
@@ -3871,7 +3878,7 @@ _renderRoundedCSSProp = function _renderRoundedCSSProp(ratio, data) {
             target.setAttribute("data-svg-origin", this.svgo || "");
         }
         i = _reverting();
-        if (i && !i.isStart && !style[_transformProp]) {
+        if ((!i || !i.isStart) && !style[_transformProp]) {
             _removeIndependentTransforms(style);
             cache.uncache = 1; // if it's a startAt that's being reverted in the _initTween() of the core, we don't need to uncache transforms. This is purely a performance optimization.
         }
@@ -3883,6 +3890,7 @@ _renderRoundedCSSProp = function _renderRoundedCSSProp(ratio, data) {
         revert: _revertStyle,
         save: _saveStyle
     };
+    target._gsap || (0, _gsapCoreJs.gsap).core.getCache(target); // just make sure there's a _gsap cache defined because we read from it in _saveStyle() and it's more efficient to just check it here once.
     properties && properties.split(",").forEach(function(p) {
         return saver.save(p);
     });
